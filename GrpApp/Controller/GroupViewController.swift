db869e07e5b122543f8642bd3b176ace9ae4e332
Asli Dogrusoz <asli.dogrusoz@gmail.com>
2018-11-20 14:15:13 +0300

    Place buttons and segues.

---
+++ GrpApp/Controller/GroupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GroupViewController.swift
+//  GrpApp
+//
+//  Created by Asli Dogrusoz on 11/20/18.
+//  Copyright © 2018 Asli Dogrusoz. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
